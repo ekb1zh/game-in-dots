0cbc62effd26866938ce9445deb0c2cc8239ce64
ekb1zh <ekb1zh@gmail.com>
2020-04-04 22:05:44 +0300

    Layout of base blocks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (null);
+  return (
+    <>
+      <div className="container">
+        awdawda 1
+      </div>
+      <div className="container">
+        awdawdaw 2
+      </div>
+    </>
+  );
 }
 
 export default App;
