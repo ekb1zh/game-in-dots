fb117453c2f9988d8ea19f577a7644e56dbb2e88
ekb1zh <ekb1zh@gmail.com>
2020-04-07 02:07:02 +0300

    Developing Grid/Square component

--- src/components/game/Grid.tsx
+++ src/components/game/Grid.tsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actions } from '../../redux';
 import * as T from "../../types";
+import { isEqualsArrays, getRandomBetween } from '../../helpers';
+
+
+
+type Coordinate = [number, number];
+type Grid = Array<Array<string>>;
+
+const colors = Object.freeze({
+  blue: 'blue',
+  green: 'green',
+  red: 'red',
+});
 
 
 function Grid() {
@@ -9,25 +21,23 @@ function Grid() {
   console.log('render Grid')
 
   const dispatch = useDispatch();
+
+  // Данные из redux
   const difficulties = useSelector<T.State, T.State['difficulties']>(state => state.difficulties);
   const currentMode = useSelector<T.State, T.State['currentMode']>(state => state.currentMode);
   const isPlaying = useSelector<T.State, T.State['isPlaying']>(state => state.isPlaying);
 
-  const startColor = 'white';
-
-  const [grid, setGrid] = useState([[startColor]]);
-
-  // !!! До тех пот пока не выбран режим сложности, таймеры нельзя включать !!!
-  const { field = 1, delay = null } = difficulties && currentMode
+  // Рабочие константы
+  const difficulty = difficulties && currentMode
     ? difficulties[currentMode]
-    : {};
+    : null;
 
-  useEffect(() => {
-    setGrid(new Array(field).fill(new Array(field).fill(startColor)));
-  }, [field]);
+  // Создание сетки
+  const [grid, setGrid] = useState<Grid | null>(null);
 
+  // Создание массива с координатами незаполненных ячеек
   const unfilledCells = useRef((() => {
-    const array = [];
+    const array: Array<Coordinate> = [];
     for (let row = 0; row < field; ++row) {
       for (let col = 0; col < field; ++col) {
         array.push([row, col]);
@@ -36,23 +46,152 @@ function Grid() {
     return array;
   })());
 
-  const timerId = useRef(null);
+  // Реакция на кнопку Play
+  const currentCoordinate = useRef<Coordinate>();
+  const timerId = useRef<number>();
 
   useEffect(() => {
-    timerId.current = setTimeout()
-  }, [isPlaying])
 
-  useEffect(() => {
-    () => clearTimeout(timerId.current)
-  }, [click])
+    if (isPlaying) {
+
+      // Выбор и запись случайной ячейки
+      const randomIndex = getRandomBetween(0, unfilledCells.current.length);
+      currentCoordinate.current = unfilledCells.current[randomIndex];
+
+      // Создание новой сетки
+      const newGrid = new Array(field)
+        .fill(null)
+        .map(el => new Array(field).fill(null)) as Grid
+
+      // Запись цвета в сетку
+      const [row, col] = currentCoordinate.current;
+      newGrid[row][col] = colors.blue;
+
+      // Запись сетки в стейт
+      setGrid(newGrid);
+
+      // Запуск таймера
+      timerId.current = window.setTimeout(timerFn, delay!);
+
+    } else {
+
+      // Сброс таймера
+      window.clearTimeout(timerId.current)
+
+      // Очистка сетки
+      setGrid(null)
+    }
+
+  }, [isPlaying]);
 
 
 
-  return (
+
+  // Функция таймера
+  function timerFn() {
+
+  }
+
+
+
+
+  // Перехватчик кликов
+  function onClick(
+    this: Coordinate,
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+
+    // Если координаты не совпали, то ничего не делать
+    if (!isEqualsArrays(currentCoordinate.current, this)) {
+      return;
+    }
+
+    // Сброс таймера
+    window.clearTimeout(timerId.current);
+
+    // Запись сетки в стейт
+    setGrid(prevGrid => {
+
+      const newGrid = [...prevGrid];
+
+      // Запись цвета в сетку
+      {
+        const [row, col] = currentCoordinate.current;
+        newGrid[row][col] = colors.green;
+      }
+
+      // Выбор и запись случайной ячейки
+      const randomIndex = getRandomBetween(0, unfilledCells.current.length);
+      currentCoordinate.current = unfilledCells.current[randomIndex];
+
+      // Запись цвета в сетку
+      const [row, col] = currentCoordinate.current;
+      newGrid[row][col] = colors.blue;
+
+      return newGrid;
+    });
+
+    // Запуск нового таймера
+    timerId.current = window.setTimeout(timerFn, delay!);
+  }
+
+
+
+
+
+  /*
+    isPlay === true
+      выбор новой ячейки
+      запись цвета в новую ячейку
+      запуск нового таймера
+
+    isPlay === false
+      сброс старого таймера
+      очистка всего
+
+    клик
+      координаты === совпали
+        сброс старого таймера
+        запись ЗЕЛЕНОГО цвета в старую ячейку
+        выбор новой ячейки
+        запись СИНЕГО цвета в новую ячейку
+        запуск нового таймера
+
+      координаты === не совпали
+        ничего не делать
+
+    окончание таймера
+      сброс старого таймера
+      выбор новой ячейки
+      запись КРАСНОГО цвета в старую ячейку
+      запись СИНЕГО цвета в новую ячейку
+      запуск нового таймера
+    
+    когда у кого-то заполненные > 50% ячеек
+      отправить результаты на сервер
+      тут же перезапросить и отобразить
+  */
+
+
+
+
+
+
+
+
+
+
+
+  return !difficulty ? null : (
     <div>
-      {grid.map(row => (
+      {grid.map((row, rowIndex) => (
         <div className='row'>
-          {row.map(color => (<div className={color} />))}
+          {row.map((color, colorIndex) => (
+            <div
+              className={color}
+              onClick={onClick.bind([rowIndex, colorIndex])}
+            />
+          ))}
         </div>
       ))}
     </div>
@@ -60,3 +199,6 @@ function Grid() {
 }
 
 export default Grid;
+
+
+
