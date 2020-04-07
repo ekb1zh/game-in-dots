bb13a99c0efb3813d728d22e232349baaf544ffb
ekb1zh <ekb1zh@gmail.com>
2020-04-07 19:43:25 +0300

    Intermediate save

--- src/components/game/Grid.tsx
+++ src/components/game/Grid.tsx
@@ -15,6 +15,15 @@ const colors = Object.freeze({
   red: 'red',
 });
 
+const enum Stage {
+  GAME_START,
+  GAME_END,
+  GAME_ABORT,
+  
+  PLAYER_SUCCESS,
+  PLAYER_TIME_OVER
+}
+
 
 function Grid() {
 
@@ -27,15 +36,21 @@ function Grid() {
   const currentMode = useSelector<T.State, T.State['currentMode']>(state => state.currentMode);
   const isPlaying = useSelector<T.State, T.State['isPlaying']>(state => state.isPlaying);
 
+  // Данные из локального стейта
+  const [grid, setGrid] = useState<Grid | null>(null);
+  // const [currentColor, setCurrentColor] = useState<string | null>(null);
+  const [currentStage, setCurrentStage] = useState(Stage.GAME_START);
+
   // Рабочие константы
-  const difficulty = difficulties && currentMode
+  const currentDifficulty = difficulties && currentMode
     ? difficulties[currentMode]
     : null;
 
-  // Создание сетки
-  const [grid, setGrid] = useState<Grid | null>(null);
+  const { field = 1, delay = null } = currentDifficulty || {};
 
-  // Создание массива с координатами незаполненных ячеек
+  // Рефы
+  const currentCoordinate = useRef<Coordinate>();
+  const timerId = useRef<number>();
   const unfilledCells = useRef((() => {
     const array: Array<Coordinate> = [];
     for (let row = 0; row < field; ++row) {
@@ -46,49 +61,87 @@ function Grid() {
     return array;
   })());
 
-  // Реакция на кнопку Play
-  const currentCoordinate = useRef<Coordinate>();
-  const timerId = useRef<number>();
 
+
+
+
+
+
+
+
+
+  // Реакция на смену данных
   useEffect(() => {
 
-    if (isPlaying) {
-
-      // Выбор и запись случайной ячейки
-      const randomIndex = getRandomBetween(0, unfilledCells.current.length);
-      currentCoordinate.current = unfilledCells.current[randomIndex];
-
-      // Создание новой сетки
-      const newGrid = new Array(field)
-        .fill(null)
-        .map(el => new Array(field).fill(null)) as Grid
-
-      // Запись цвета в сетку
-      const [row, col] = currentCoordinate.current;
-      newGrid[row][col] = colors.blue;
-
-      // Запись сетки в стейт
-      setGrid(newGrid);
-
-      // Запуск таймера
-      timerId.current = window.setTimeout(timerFn, delay!);
-
-    } else {
-
-      // Сброс таймера
-      window.clearTimeout(timerId.current)
-
-      // Очистка сетки
-      setGrid(null)
+    // Сброс таймера
+    if (timerId.current) {
+      window.clearTimeout(timerId.current);
     }
 
-  }, [isPlaying]);
+    // Перезапись сетки
+    setGrid(!isPlaying
+      ? null
+      : prevGrid => {
+
+        const newGrid = prevGrid
+          ? [...prevGrid]
+          : new Array(field)
+            .fill(null)
+            .map(el => new Array(field).fill(null)) as Grid;
+
+        // Запись текущего цвета в текущую ячейку
+        {
+          const [row, col] = currentCoordinate.current;
+          newGrid[row][col] = currentColor;
+        }
+
+        // Выбор новой случайной ячейки
+        const randomIndex = getRandomBetween(0, unfilledCells.current.length);
+        currentCoordinate.current = unfilledCells.current[randomIndex];
+
+        // Удаление ячейки из масссива незаполненных
+        // !!!!!!!!!!!!!!!!!
+
+        // Запись синего цвета в новую ячейку
+        {
+          const [row, col] = currentCoordinate.current;
+          newGrid[row][col] = colors.blue;
+        }
+
+        return newGrid;
+      }
+    );
+
+    // Перезапись цвета 
+    setCurrentColor(...)
+
+    // Запуск нового таймера
+    if(delay) {
+      timerId.current = window.setTimeout(timerFn, delay);
+    }
+
+  }, [isPlaying, currentColor]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
   // Функция таймера
   function timerFn() {
+
 
   }
 
@@ -106,33 +159,7 @@ function Grid() {
       return;
     }
 
-    // Сброс таймера
-    window.clearTimeout(timerId.current);
 
-    // Запись сетки в стейт
-    setGrid(prevGrid => {
-
-      const newGrid = [...prevGrid];
-
-      // Запись цвета в сетку
-      {
-        const [row, col] = currentCoordinate.current;
-        newGrid[row][col] = colors.green;
-      }
-
-      // Выбор и запись случайной ячейки
-      const randomIndex = getRandomBetween(0, unfilledCells.current.length);
-      currentCoordinate.current = unfilledCells.current[randomIndex];
-
-      // Запись цвета в сетку
-      const [row, col] = currentCoordinate.current;
-      newGrid[row][col] = colors.blue;
-
-      return newGrid;
-    });
-
-    // Запуск нового таймера
-    timerId.current = window.setTimeout(timerFn, delay!);
   }
 
 
@@ -182,7 +209,7 @@ function Grid() {
 
 
 
-  return !difficulty ? null : (
+  return !currentDifficulty ? null : (
     <div>
       {grid.map((row, rowIndex) => (
         <div className='row'>
