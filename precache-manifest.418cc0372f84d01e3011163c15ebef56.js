993d0c56c3cf528e43d16b626157d9ee39850996
ekb1zh <ekb1zh@gmail.com>
2020-04-12 19:15:42 +0300

    Updates

---
+++ precache-manifest.418cc0372f84d01e3011163c15ebef56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c264a2e9f1db398a4589aeadccce1b7e",
+    "url": "/game-in-dots/index.html"
+  },
+  {
+    "revision": "7a76c268087223982b3b",
+    "url": "/game-in-dots/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9f0c8e7b3fd52b62ecfc",
+    "url": "/game-in-dots/static/css/main.07dc8d45.chunk.css"
+  },
+  {
+    "revision": "7a76c268087223982b3b",
+    "url": "/game-in-dots/static/js/2.287f4bd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-in-dots/static/js/2.287f4bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0c8e7b3fd52b62ecfc",
+    "url": "/game-in-dots/static/js/main.88daf2ae.chunk.js"
+  },
+  {
+    "revision": "52e6d185a13275c72550",
+    "url": "/game-in-dots/static/js/runtime-main.6d5097c9.js"
+  }
+]);
