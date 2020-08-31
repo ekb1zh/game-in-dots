eb39c00609eee22be1a11c29065af966de8a48a9
ekb1zh <ekb1zh@gmail.com>
2020-09-01 02:05:52 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-in-dots/precache-manifest.1dc2813f8a82e39c096b2933d5e102a4.js"
+  "/game-in-dots/precache-manifest.4d2ec1a9bb05e1f11302fdefc2273d77.js"
 );
 
 self.addEventListener('message', (event) => {
